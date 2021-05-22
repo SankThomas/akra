fff03b8282c1ad1094be894b5cdd553a3ec4914a
Thomas Sankara <thomassankara74@gmail.com>
2021-05-22 18:42:18 +0300

    style left side navbar

--- src/components/NavbarLeft.js
+++ src/components/NavbarLeft.js
@@ -2,15 +2,15 @@ import { useState } from "react"
 import { NavLink } from "react-router-dom"
 import { navbarLeft } from "../data/navbar"
 
-const NavbarLeft = () => {
+const NavbarLeft = ({ setIsOpen }) => {
   // eslint-disable-next-line
   const [leftLinks, setLeftLinks] = useState(navbarLeft)
 
   return (
     <>
-      <ul>
+      <ul className="flex flex-wrap items-center justify-center absolute w-72 left-9 top-20 bg-white shadow-md p-5 rounded-lg">
         {leftLinks.map(({ id, title, url }) => (
-          <li key={id}>
+          <li key={id} onClick={() => setIsOpen(false)} className="m-5 text-sm">
             <NavLink to={url}>{title}</NavLink>
           </li>
         ))}
