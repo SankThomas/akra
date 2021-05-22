1ddcbbe033be6c277a1d6afc88f33822d4a80e37
Thomas Sankara <thomassankara74@gmail.com>
2021-05-22 17:31:38 +0300

    Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import logo from "../assets/akra.svg"
+
+const Logo = () => {
+  return (
+    <>
+      <img src={logo} alt="Akra logo" title="Akra Ecommerce" />
+    </>
+  )
+}
+
+export default Logo
