871c7ccfb022fbe6bc7ca8add5261171ad531784
Thomas Sankara <thomassankara74@gmail.com>
2021-05-25 19:06:02 +0300

    Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import NavbarLeft from "./NavbarLeft"
+import NavbarRight from "./NavbarRight"
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <NavbarLeft />
+      </nav>
+
+      <nav>
+        <NavbarRight />
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
